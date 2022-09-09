fed9e0a6b873c12d74b6f3193ceae78fde780d13
dominic <domw30@gmail.com>
2022-09-09 11:57:26 +0100

    feat: add eslint configuration and resolve linting warnings

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
+	presets: [
+		['@babel/preset-env', { targets: { node: 'current' } }],
+		'@babel/preset-typescript',
+		'@babel/preset-react',
+	],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['src'],
+				alias: {
+					'@features': 'src/features',
+					'@pages': 'src/pages',
+					'@lib': 'src/lib',
+					'@types': 'src/lib/types',
+				},
+			},
+		],
+	],
 };
