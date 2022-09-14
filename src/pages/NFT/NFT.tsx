ee0a6aa2b0f28ebe328b1017c53559363bf64a5f
dominic <domw30@gmail.com>
2022-09-14 16:59:42 +0100

    fix: remove temporary development and testing additions

--- src/pages/NFT/NFT.tsx
+++ src/pages/NFT/NFT.tsx
@@ -11,7 +11,6 @@ import { DomainPreview } from '../../features/domain-preview';
 import { Domain, DomainMetrics, TokenPriceInfo } from '@zero-tech/zns-sdk';
 
 import { Metadata } from '../../lib/types/metadata';
-import { TransferOwnershipModal } from '../../features/transfer-ownership';
 
 type NFTProps = {
 	domain: Domain;
@@ -25,47 +24,30 @@ export const NFT: FC<NFTProps> = ({
 	metrics,
 	domainMetadata,
 	paymentTokenInfo,
-}) => {
-	const [isOpen, setIsOpen] = useState(true);
-	const onClose = () => setIsOpen(false);
-	return (
-		<>
-			<DomainPreview
-				title={domainMetadata?.title}
-				description={domainMetadata?.description}
-				owner={domain?.owner}
-				creator={domain?.minter}
-				isNFTView
-			/>
+}) => (
+	<>
+		<DomainPreview
+			title={domainMetadata?.title}
+			description={domainMetadata?.description}
+			owner={domain?.owner}
+			creator={domain?.minter}
+			isNFTView
+		/>
 
-			<Actions
-				domain={domain}
-				domainMetadata={domainMetadata}
-				paymentTokenInfo={paymentTokenInfo}
-			/>
+		<Actions
+			domain={domain}
+			domainMetadata={domainMetadata}
+			paymentTokenInfo={paymentTokenInfo}
+		/>
 
-			<NFTMetrics
-				domainId={domain?.id}
-				metrics={metrics}
-				paymentTokenInfo={paymentTokenInfo}
-			/>
+		<NFTMetrics
+			domainId={domain?.id}
+			metrics={metrics}
+			paymentTokenInfo={paymentTokenInfo}
+		/>
 
-			{/* todo: remove - temp import to display modal for development prior to dropdown implementation */}
-			{domain && domainMetadata && (
-				<TransferOwnershipModal
-					domainId={domain.id}
-					domainName={domain.name}
-					domainTitle={domainMetadata.title}
-					domainOwner={domain.owner}
-					domainCreator={domain.minter}
-					open={isOpen}
-					onClose={onClose}
-				/>
-			)}
+		<TokenHashInfo domain={domain} />
 
-			<TokenHashInfo domain={domain} />
-
-			<HistoryList domainId={domain?.id} paymentToken={paymentTokenInfo} />
-		</>
-	);
-};
+		<HistoryList domainId={domain?.id} paymentToken={paymentTokenInfo} />
+	</>
+);
