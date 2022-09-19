b391d45bda7878ca92908ee10817005f01f608c0
dominic <domw30@gmail.com>
2022-09-19 12:05:45 +0100

    refactor: update mock address'

--- src/features/transfer-ownership/TransferOwnership.mocks.ts
+++ src/features/transfer-ownership/TransferOwnership.mocks.ts
@@ -1,11 +1,11 @@
-export const validInputAddress = '0xBcd9DD2B8693b316F0f4690bb069314A7866cc59';
+export const validInputAddress = '0x0ec78ed49c2d27b315d462d43b5bab94d2c79bf8';
 export const inValidInputAddress = '0x-not-a-//-valid-eth-!!@-address';
 
-export const connectedAccount = '0x07774f5650CEaf4E6bc026caaf5A474b2E9c7334';
+export const connectedAccount = '0x13fc7bcca25bc0bab1c9cbec2ea2a254f4357f6e';
 
 export const domain = {
 	id: '0xada136a490b49f140280941197b1c56cdc9668ec9c8b515c8f00d116b9942c09',
 	name: 'wilder.snowflake',
-	owner: '0x07774f5650CEaf4E6bc026caaf5A474b2E9c7334',
+	owner: '0x13fc7bcca25bc0bab1c9cbec2ea2a254f4357f6e',
 	minter: '0x7829afa127494ca8b4ceef4fb81b78fee9d0e471',
 };
