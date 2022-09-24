57c03d085a8462418a73f0e4653c0144b423e62f
Ash Midgley <ashleymidgley@gmail.com>
2022-09-24 14:41:13 +0800

    Remove extras.

--- src/features/subdomain-view/subdomain-table/SubdomainTable.constants.ts
+++
@@ -1,8 +0,0 @@
-//- Components Imports
-import { Column } from '@zero-tech/zui/src/components/AsyncTable';
-
-export const COLUMNS: Column[] = [
-	{ id: 'domain', header: 'Domain', alignment: 'left' },
-	{ id: 'volume', header: 'Volume(all time)', alignment: 'right' },
-	{ id: 'action', header: '', alignment: 'right' },
-];
--- src/features/ui/NFTCard/NFTCard.constants.ts
+++
@@ -1,8 +0,0 @@
-export const AddressTitle = {
-	CREATOR: 'Creator',
-	OWNER: 'Owner',
-};
-
-export const ButtonTitle = {
-	VIEW_DOMAIN_NFT: 'View Domain NFT ->',
-};
