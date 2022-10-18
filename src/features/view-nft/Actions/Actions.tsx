a61ed498b4899ef7b6b442d7707d7ae7e7620ead
dominic <domw30@gmail.com>
2022-10-18 11:15:13 +0100

    fix: fix undefined nft actions data

--- src/features/view-nft/Actions/Actions.tsx
+++ src/features/view-nft/Actions/Actions.tsx
@@ -1,5 +1,3 @@
-import { Domain, TokenPriceInfo } from '@zero-tech/zns-sdk';
-
 import { Action } from '..';
 import { BuyNowButton } from '../../buy-now';
 import { SetBuyNowButton } from '../../set-buy-now';
@@ -7,85 +5,67 @@ import { PlaceBidButton } from '../../place-bid';
 import { ViewBidsButton } from '../../view-bids';
 import { CancelBidButton } from '../../cancel-bid';
 
-import { DataTestId, Messages } from './Actions.constants';
-import { Labels } from '../../../lib/constants/labels';
-
 import { useWeb3 } from '../../../lib/hooks/useWeb3';
 import { useBuyNowPrice } from '../../../lib/hooks/useBuyNowPrice';
 import { useBidData } from '../../../lib/hooks/useBidData';
-
-import {
-	getOrderedActions,
-	getUsdConversion,
-	getVisibleActions,
-} from './Actions.utils';
-import { formatNumber } from '../../../lib/util/number/number';
-import { getHighestBid, getUserBids } from '../../../lib/util/bids/bids';
-
-import { ActionBlock, ActionTypes } from './Actions.types';
+import { useDomainMetrics } from '../../../lib/hooks/useDomainMetrics';
+import { formatEthers, formatNumber } from '../../../lib/util/number/number';
+import { getUserBids } from '../../../lib/util/bids/bids';
 import { Metadata } from '../../../lib/types/metadata';
+import { Labels } from '../../../lib/constants/labels';
+import { Domain } from '@zero-tech/zns-sdk';
+
+import { DataTestId } from './Actions.constants';
+import { ActionBlock, ActionTypes } from './Actions.types';
+import { getOrderedActions, getVisibleActions } from './Actions.utils';
 
 import styles from './Actions.module.scss';
 
 type ActionsProps = {
 	domain?: Domain;
 	domainMetadata?: Metadata;
-	paymentTokenInfo?: TokenPriceInfo;
 };
 
-export const Actions = ({
-	domain,
-	domainMetadata,
-	paymentTokenInfo,
-}: ActionsProps) => {
+export const Actions = ({ domain, domainMetadata }: ActionsProps) => {
 	const { account } = useWeb3();
-	const { data: buyNowPrice } = useBuyNowPrice(domain?.id);
+	const { data: buyNowPriceData } = useBuyNowPrice(domain?.id);
+	const { data: metrics } = useDomainMetrics(domain?.id);
 	const { data: bids } = useBidData(domain?.id);
-	const userBids = getUserBids(account, bids);
-	const highestBid = getHighestBid(bids);
-	const highestUserBid = getHighestBid(userBids);
+
+	const buyNowPrice = buyNowPriceData ? formatNumber(buyNowPriceData) : '-';
+	const userBids = getUserBids(account, bids) ?? [];
+	const highestUserBid =
+		userBids.length > 0 ? formatEthers(userBids[0]?.amount) : '-';
+	const highestBid =
+		metrics?.highestBid > '0' ? formatEthers(metrics?.highestBid) : '-';
+
 	const isOwnedByUser = domain?.owner?.toLowerCase() === account?.toLowerCase();
 	const isBiddable = !isOwnedByUser || Boolean(domainMetadata?.isBiddable);
-	const isUserBid = !isOwnedByUser && Boolean(highestUserBid);
+	const isUserBid = !isOwnedByUser && userBids.length > 0;
 	const isSetBuyNow = isOwnedByUser && Boolean(domain?.name);
 	const isBuyNow =
-		Boolean(buyNowPrice) && !isOwnedByUser && Boolean(domain?.name);
+		Boolean(buyNowPriceData) && !isOwnedByUser && Boolean(domain?.name);
 	const isViewBids =
 		isOwnedByUser !== undefined && isBiddable && bids?.length > 0;
 
 	const actions: { [action in ActionTypes]: ActionBlock } = {
 		[ActionTypes.BUY_NOW]: {
-			label: `${Labels.BUY_NOW} (${paymentTokenInfo?.name})`,
-			amountToken: formatNumber(buyNowPrice),
-			amountUsd: getUsdConversion(
-				buyNowPrice,
-				paymentTokenInfo?.price,
-				Messages.NO_BUY_NOW,
-			),
+			label: `${Labels.BUY_NOW}`,
+			amountToken: buyNowPrice,
 			isVisible: isBuyNow,
 			dataTestId: DataTestId.BUY_NOW,
 			buttonComponent: <BuyNowButton />,
 		},
 		[ActionTypes.SET_BUY_NOW]: {
-			label: `${Labels.BUY_NOW} (${paymentTokenInfo?.name})`,
-			amountToken: buyNowPrice ? formatNumber(buyNowPrice) : '-',
-			amountUsd: getUsdConversion(
-				buyNowPrice,
-				paymentTokenInfo?.price,
-				Messages.NO_BUY_NOW,
-			),
+			label: `${Labels.BUY_NOW}`,
+			amountToken: buyNowPrice,
 			isVisible: isSetBuyNow,
 			dataTestId: DataTestId.SET_BUY_NOW,
 			buttonComponent: <SetBuyNowButton />,
 		},
 		[ActionTypes.BID]: {
-			label: `${Labels.HIGHEST_BID} (${paymentTokenInfo?.name})`,
-			amountToken: highestBid > 0 ? highestBid : '-',
-			amountUsd: getUsdConversion(
-				highestBid,
-				paymentTokenInfo?.price,
-				Messages.NO_BIDS_PLACED,
-			),
+			label: `${Labels.HIGHEST_BID}`,
+			amountToken: highestBid,
 			isVisible: isBiddable || isViewBids,
 			dataTestId: DataTestId.BID,
 			buttonComponent: !isOwnedByUser ? (
@@ -97,9 +77,8 @@ export const Actions = ({
 			),
 		},
 		[ActionTypes.USER_BID]: {
-			label: `${Labels.YOUR_BID} (${paymentTokenInfo?.name})`,
-			amountToken: highestUserBid ? highestUserBid : '-',
-			amountUsd: getUsdConversion(highestBid, paymentTokenInfo?.price),
+			label: `${Labels.YOUR_BID}`,
+			amountToken: highestUserBid,
 			isVisible: isUserBid,
 			dataTestId: DataTestId.USER_BID,
 			buttonComponent: <CancelBidButton />,
