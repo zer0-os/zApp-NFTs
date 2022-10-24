5a0a3a036f75fb96cd8ae8bc90fb942ada583df2
dominic <domw30@gmail.com>
2022-10-24 12:34:12 +0100

    feat: add variation to view bids button pt 2

--- src/features/view-bids/ViewBidsButton.tsx
+++ src/features/view-bids/ViewBidsButton.tsx
@@ -1,5 +1,22 @@
 import { FC } from 'react';
 
 import { ViewBidsModal } from './ViewBidsModal';
+import { Button } from '@zero-tech/zui/components';
+import { IconLinkExternal1 } from '@zero-tech/zui/components/Icons';
 
-export const ViewBidsButton: FC = () => <ViewBidsModal trigger={'View Bids'} />;
+import styles from './ViewBidsButton.module.scss';
+
+interface ViewBidsButtonProps {
+	isTextButton?: boolean;
+}
+
+export const ViewBidsButton: FC<ViewBidsButtonProps> = ({ isTextButton }) => {
+	const textButton = (
+		<div className={styles.Container}>
+			<p className={styles.TextButton}>View All Bids</p>
+			<IconLinkExternal1 className={styles.Icon} size={'1rem'} isFilled />
+		</div>
+	);
+
+	return <ViewBidsModal trigger={isTextButton ? textButton : 'View Bids'} />;
+};
