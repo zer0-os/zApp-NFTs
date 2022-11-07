93a5d543662d53662d8365669531b779243f3abe
dominic <domw30@gmail.com>
2022-11-07 09:25:32 +0000

    refactor: rename payment label and reorder imports

--- src/features/view-nft/NFTMetrics/NFTMetrics.tsx
+++ src/features/view-nft/NFTMetrics/NFTMetrics.tsx
@@ -1,13 +1,12 @@
 import { FC } from 'react';
 
-import { StatsList } from '../../ui';
-
 import { useBidData } from '../../../lib/hooks/useBidData';
-
-import { formatEthers, formatNumber } from '../../../lib/util/number/number';
-import { getDomainId, getParentZna } from '../../../lib/util/domains/domains';
+import { formatEthers, formatNumber } from '../../../lib/util/number';
+import { getDomainId, getParentZna } from '../../../lib/util/domains';
 import { usePaymentToken } from '../../../lib/hooks/usePaymentToken';
 import { useDomainMetrics } from '../../../lib/hooks/useDomainMetrics';
+
+import { StatsList } from '../../ui';
 
 type NFTMetricsProps = {
 	zna: string;
@@ -17,21 +16,23 @@ export const NFTMetrics: FC<NFTMetricsProps> = ({ zna }) => {
 	const domainId = getDomainId(zna);
 	const parentZna = getParentZna(zna);
 
+	const { data: paymentToken } = usePaymentToken(parentZna);
 	const { data: bids, isLoading: isLoadingBids } = useBidData(domainId);
 	const { data: metrics, isLoading: isLoadingMetrics } =
 		useDomainMetrics(domainId);
-	const { data: paymentToken } = usePaymentToken(parentZna);
 
 	let numberOfBids, lastSale, volumeString;
+
 	if (!isLoadingBids && bids) {
 		numberOfBids = formatNumber(bids.length || 0).toLocaleString();
 	}
+
 	if (!isLoadingMetrics && metrics) {
 		lastSale = formatEthers(metrics.lastSale);
 		volumeString = formatEthers(metrics.volume.all);
 	}
 
-	const paymentTokenSymbol = paymentToken?.label ?? '';
+	const paymentTokenLabel = paymentToken?.label ?? '';
 
 	const stats = [
 		{
@@ -42,14 +43,14 @@ export const NFTMetrics: FC<NFTMetricsProps> = ({ zna }) => {
 			},
 		},
 		{
-			title: 'Last Sale ' + paymentTokenSymbol,
+			title: `Last Sale ${paymentTokenLabel}`,
 			value: {
 				isLoading: isLoadingMetrics,
 				text: lastSale,
 			},
 		},
 		{
-			title: 'Volume ' + paymentTokenSymbol,
+			title: `Volume ${paymentTokenLabel}`,
 			value: {
 				isLoading: isLoadingMetrics,
 				text: volumeString,
