786e0a3c6127443768ade4faee9e3f30e38475e6
Subrahmanyam <subrahmanyam03@gmail.com>
2022-11-09 00:36:06 +0530

    feat(goerli-migration): add support for goerli test network

--- src/lib/constants/networks.ts
+++ src/lib/constants/networks.ts
@@ -1,16 +1,16 @@
 export enum Network {
 	MAINNET = 1,
-	RINKEBY = 4,
+	GOERLI = 5,
 }
 
 export enum NETWORK_TYPES {
 	MAINNET = 'MAINNET',
-	RINKEBY = 'RINKEBY',
+	GOERLI = 'GOERLI',
 	LOCAL = 'LOCAL',
 }
 
 const ENV_NETWORK = process.env.REACT_APP_DEFAULT_NETWORK;
-export const DEFAULT_NETWORK = Network.MAINNET;
+export const DEFAULT_NETWORK = Network.GOERLI;
 
 interface NetworkConfig {
 	rpcUrl: string;
@@ -18,14 +18,14 @@ interface NetworkConfig {
 
 export const RPC_URLS: { [chainId: number]: string } = {
 	1: process.env.REACT_APP_INFURA_URL as string,
-	4: process.env.REACT_APP_INFURA_URL as string,
+	5: process.env.REACT_APP_INFURA_URL as string,
 };
 
 export const NETWORK_CONFIGS: { [network in Network]: NetworkConfig } = {
 	[Network.MAINNET]: {
 		rpcUrl: RPC_URLS[1],
 	},
-	[Network.RINKEBY]: {
-		rpcUrl: RPC_URLS[4],
+	[Network.GOERLI]: {
+		rpcUrl: RPC_URLS[5],
 	},
 };
