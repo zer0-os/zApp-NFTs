f604e3d558c4618d4a7c7c737706ad2a63732f09
dominic <domw30@gmail.com>
2022-11-09 14:51:06 +0000

    feat: improve create token modal

--- src/features/create-token/CreateTokenForm.tsx
+++ src/features/create-token/CreateTokenForm.tsx
@@ -1,13 +1,10 @@
 import { FC } from 'react';
 
-import { useWeb3 } from '../../lib/hooks/useWeb3';
-
 import {
 	CreateTokenBody,
 	CreateTokenHeader,
 	CreateTokenFormContextProvider,
 } from './';
-import { ConnectWallet } from '../ui/ConnectWallet';
 import { Wizard } from '@zero-tech/zui/components';
 
 import styles from './CreateToken.module.scss';
@@ -18,18 +15,12 @@ export type CreateTokenFormProps = {
 };
 
 export const CreateTokenForm: FC<CreateTokenFormProps> = ({ zna, onClose }) => {
-	const { account } = useWeb3();
-
-	const content = account ? (
+	return (
 		<CreateTokenFormContextProvider>
 			<Wizard.Container className={styles.Container}>
 				<CreateTokenHeader subtitle={zna} onClose={onClose} />
 				<CreateTokenBody onClose={onClose} />
 			</Wizard.Container>
 		</CreateTokenFormContextProvider>
-	) : (
-		<ConnectWallet message={'Connect your wallet to create a token.'} />
 	);
-
-	return content;
 };
--- src/features/create-token/CreateTokenModal.tsx
+++ src/features/create-token/CreateTokenModal.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react';
 
+import { useWeb3 } from '../../lib/hooks/useWeb3';
 import { BasicModalProps } from '../../lib/types/ui';
 
+import { CreateTokenForm, CreateTokenFormProps } from './';
+import { ConnectWallet } from '../ui/ConnectWallet';
 import { Modal } from '@zero-tech/zui/components';
-import { CreateTokenForm } from './';
 
 export interface CreateTokenModalProps extends BasicModalProps {
-	zna: string;
-	onClose: () => string;
+	zna: ModalContentProps['zna'];
+	onClose: ModalContentProps['onClose'];
 }
 
 export const CreateTokenModal: FC<CreateTokenModalProps> = ({
@@ -17,9 +19,29 @@ export const CreateTokenModal: FC<CreateTokenModalProps> = ({
 	onOpenChange,
 	...props
 }) => {
+	const { account } = useWeb3();
+
 	return (
 		<Modal {...props} open={open} onOpenChange={onOpenChange}>
-			<CreateTokenForm zna={zna} onClose={onClose} />
+			<ModalContent account={account} zna={zna} onClose={onClose} />
 		</Modal>
+	);
+};
+
+/*******************
+ * ModalContent
+ *******************/
+
+interface ModalContentProps {
+	account: string;
+	zna: CreateTokenFormProps['zna'];
+	onClose: CreateTokenFormProps['onClose'];
+}
+
+const ModalContent = ({ account, zna, onClose }: ModalContentProps) => {
+	return account ? (
+		<CreateTokenForm zna={zna} onClose={onClose} />
+	) : (
+		<ConnectWallet message={'Connect your wallet to create a token.'} />
 	);
 };
