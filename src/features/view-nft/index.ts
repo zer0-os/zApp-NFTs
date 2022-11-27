c48446e60f192764b859ef713323c5cbf18b1708
DomW <domw30@gmail.com>
2022-11-27 03:43:05 +0000

    feat: nft view page image and container improvements (#54)
    * feat: initial changes for image and containers
    
    * refactor: add todo's for follow up task
    
    * refactor: rename components to align with figma designs
    
    * refactor: set max-height of image
    
    * refactor: improve page layouts
    
    * feat: implement prominent colour picking tool
    
    * feat: add media queries for image alignments

--- src/features/view-nft/index.ts
+++ src/features/view-nft/index.ts
@@ -2,5 +2,7 @@ export * from './Action';
 export * from './Actions';
 export * from './HistoryItem';
 export * from './HistoryList';
+export * from './NFTBannerContainer';
+export * from './NFTDetailsCard';
 export * from './NFTMetrics';
 export * from './TokenHashInfo';
