274dd1041cb9acb41e619c853b87ceb2825385ed
dominic <domw30@gmail.com>
2023-01-10 20:31:46 +0000

    feat: add toggle switches to details step

--- src/features/domain-settings/DomainSettingsForm/FormSteps/Details/Details.tsx
+++ src/features/domain-settings/DomainSettingsForm/FormSteps/Details/Details.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import { useDomainSettingsData } from '../../../useDomainSettingsData';
 
 import { Switch } from '../../ui';
-import { Input } from '@zero-tech/zui/components';
+import { Button, Input } from '@zero-tech/zui/components';
 import { IpfsMedia } from '@zero-tech/zapp-utils/components';
 import { InfoTooltip } from '@zero-tech/zui/components/InfoTooltip';
 
@@ -18,72 +18,79 @@ export const Details: FC<DetailsProps> = ({ zna, errorText }) => {
 	const { description, imageAlt, imageSrc } = useDomainSettingsData(zna);
 
 	return (
-		<div className={styles.Container}>
-			<div className={styles.FlexRowWrapper}>
-				<Media alt={imageAlt} src={imageSrc} />
-				<InputGroup zna={zna} />
-			</div>
+		<>
+			<div className={styles.Container}>
+				<div className={styles.FlexRowWrapper}>
+					<Media alt={imageAlt} src={imageSrc} />
+					<InputGroup zna={zna} />
+				</div>
 
-			<TextArea description={description} />
+				<TextArea description={description} />
 
-			<div className={styles.AdvancedSettings}>
-				<h5>Advanced Domain Settings</h5>
+				<div className={styles.AdvancedSettings}>
+					<h5>Advanced Domain Settings</h5>
 
-				<div className={styles.SwitchGroup}>
-					<div className={styles.SwitchRow}>
-						<Switch
-							label={'Domain Mint Requests'}
-							toggled={false}
-							onPress={() => {}}
-						/>
-						<InfoTooltip
-							content={
-								'Allow members to make a stake offer in order to mint NFTs on your domain. Turn off if your domain is not intended to be open for others to mint upon'
-							}
-						/>
-					</div>
+					<div className={styles.SwitchGroup}>
+						<div className={styles.SwitchRow}>
+							<Switch
+								label={'Domain Mint Requests'}
+								toggled={false}
+								onPress={() => {}}
+							/>
+							<InfoTooltip
+								content={
+									'Allow members to make a stake offer in order to mint NFTs on your domain. Turn off if your domain is not intended to be open for others to mint upon'
+								}
+							/>
+						</div>
 
-					<div className={styles.SwitchRow}>
-						<Switch
-							label={'Domain Bidding'}
-							toggled={false}
-							onPress={() => {}}
-						/>
-						<InfoTooltip
-							content={
-								'Allow bidding on your domain. Turn off if the domain is not intended to be sold.'
-							}
-						/>
-					</div>
+						<div className={styles.SwitchRow}>
+							<Switch
+								label={'Domain Bidding'}
+								toggled={false}
+								onPress={() => {}}
+							/>
+							<InfoTooltip
+								content={
+									'Allow bidding on your domain. Turn off if the domain is not intended to be sold.'
+								}
+							/>
+						</div>
 
-					<div className={styles.SwitchRow}>
-						<Switch
-							label={'Domain in Grid View by Default'}
-							toggled={false}
-							onPress={() => {}}
-						/>
-						<InfoTooltip
-							content={
-								'Grid view has larger image previews which can benefit domains with a focus on art rather than statistics.'
-							}
-						/>
-					</div>
+						<div className={styles.SwitchRow}>
+							<Switch
+								label={'Domain in Grid View by Default'}
+								toggled={false}
+								onPress={() => {}}
+							/>
+							<InfoTooltip
+								content={
+									'Grid view has larger image previews which can benefit domains with a focus on art rather than statistics.'
+								}
+							/>
+						</div>
 
-					<div className={styles.SwitchRow}>
-						<Switch
-							label={'Custom Domain Header'}
-							toggled={false}
-							onPress={() => {}}
-						/>
-						<InfoTooltip
-							content={
-								'Change the first column header of list view. By default this is ‘Domain’.'
-							}
-						/>
+						<div className={styles.SwitchRow}>
+							<Switch
+								label={'Custom Domain Header'}
+								toggled={false}
+								onPress={() => {}}
+							/>
+							<InfoTooltip
+								content={
+									'Change the first column header of list view. By default this is ‘Domain’.'
+								}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<div className={styles.Footer}>
+				<Button>Save</Button>
+				<Button>Save</Button>
+			</div>
+		</>
 	);
 };
 
