2605ff54c3285e3697816534350845b84baeb78b
dominic <domw30@gmail.com>
2023-01-23 11:28:34 +0000

    refactor: tidy up more options file and include view bids to dropdown options

--- src/features/ui/MoreNFTOptions/MoreNFTOptions.tsx
+++ src/features/ui/MoreNFTOptions/MoreNFTOptions.tsx
@@ -1,6 +1,7 @@
 import { FC, useCallback, ReactNode, useState } from 'react';
 
 import { OptionLabel } from '../OptionLabel';
+import { ViewBidsModal } from '../../view-bids';
 import { SetBuyNowModal } from '../../set-buy-now';
 import { CreateTokenModal } from '../../create-token';
 import { DomainSettingsModal } from '../../domain-settings';
@@ -12,6 +13,7 @@ import {
 	IconSend3,
 	IconCube1,
 	IconDatabase2,
+	IconCurrencyDollar,
 } from '@zero-tech/zui/components/Icons';
 
 export const enum OptionType {
@@ -19,35 +21,20 @@ export const enum OptionType {
 	DOMAIN_SETTINGS = 'domain-settings',
 	SET_BUY_NOW = 'set-buy-now',
 	TRANSFER = 'transfer',
+	VIEW_BIDS = 'view-bids',
 }
 
 export type Option =
 	| 'create-token'
 	| 'domain-settings'
 	| 'set-buy-now'
-	| 'transfer';
+	| 'transfer'
+	| 'view-bids';
 
 type MoreNFTOptionsProps = {
 	zna: string;
 	trigger: ReactNode;
 };
-
-// TODO: add option label to zUI
-const createTokenOptionLabel = (
-	<OptionLabel icon={<IconDatabase2 isFilled />} label="Create Token" />
-);
-
-const domainSettingsOptionLabel = (
-	<OptionLabel icon={<IconCube1 isFilled />} label="My Domain Settings" />
-);
-
-const setBuyNowOptionLabel = (
-	<OptionLabel icon={<IconTag1 isFilled />} label="Set Buy Now" />
-);
-
-const transferOptionLabel = (
-	<OptionLabel icon={<IconSend3 isFilled />} label="Transfer Ownership" />
-);
 
 /**
  * Wraps the shared functionality of additional NFT options.
@@ -57,27 +44,50 @@ export const MoreNFTOptions: FC<MoreNFTOptionsProps> = ({ zna, trigger }) => {
 
 	const moreOptions = [
 		{
+			className: 'view-bids',
+			id: OptionType.VIEW_BIDS,
+			label: (
+				<OptionLabel
+					label={'View Bids'}
+					icon={<IconCurrencyDollar isFilled />}
+				/>
+			),
+			onSelect: (e: any) => onSelectOption(e),
+		},
+		{
 			className: 'create-token',
 			id: OptionType.CREATE_TOKEN,
-			label: createTokenOptionLabel,
+			label: (
+				<OptionLabel label={'Create Token'} icon={<IconDatabase2 isFilled />} />
+			),
 			onSelect: (e: any) => onSelectOption(e),
 		},
 		{
 			className: 'domain-settings',
 			id: OptionType.DOMAIN_SETTINGS,
-			label: domainSettingsOptionLabel,
+			label: (
+				<OptionLabel
+					label={'My Domain Settings'}
+					icon={<IconCube1 isFilled />}
+				/>
+			),
 			onSelect: (e: any) => onSelectOption(e),
 		},
 		{
 			className: 'set-buy-now',
 			id: OptionType.SET_BUY_NOW,
-			label: setBuyNowOptionLabel,
+			label: <OptionLabel label={'Set Buy Now'} icon={<IconTag1 isFilled />} />,
 			onSelect: (e: any) => onSelectOption(e),
 		},
 		{
 			className: 'transfer',
 			id: OptionType.TRANSFER,
-			label: transferOptionLabel,
+			label: (
+				<OptionLabel
+					label={'Transfer Ownership'}
+					icon={<IconSend3 isFilled />}
+				/>
+			),
 			onSelect: (e: any) => onSelectOption(e),
 		},
 	];
@@ -129,6 +139,12 @@ export const MoreNFTOptions: FC<MoreNFTOptionsProps> = ({ zna, trigger }) => {
 				open={option === OptionType.TRANSFER}
 				onOpenChange={onChange}
 				onClose={onClose}
+			/>
+
+			<ViewBidsModal
+				zna={zna}
+				open={option === OptionType.VIEW_BIDS}
+				onOpenChange={onChange}
 			/>
 
 			<DropdownMenu
