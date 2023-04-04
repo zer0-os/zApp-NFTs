e09cdc85f0ce87e93e14a2c941bd9ad42ed4aca5
Subrahmanyam <subrahmanyam03@gmail.com>
2023-04-04 17:59:45 +0530

    feat(gens-drop): add zsale sdk provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { AppProps } from './lib/types/app';
 import { ChainGate } from './lib/util/ChainGate';
 import { Web3Provider } from './lib/providers/Web3Provider';
 import { ZnsSdkProvider } from './lib/providers/ZnsSdkProvider';
+import { ZsaleSdkProvider } from './lib/providers/ZsaleSdkProvider';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import { ZUIProvider } from '@zero-tech/zui/ZUIProvider';
 
@@ -13,19 +15,21 @@ const queryClient = new QueryClient();
 
 export const NFTsZApp = ({ provider, web3 }: AppProps) => (
 	<QueryClientProvider client={queryClient} contextSharing={true}>
-		<Web3Provider
-			provider={provider}
-			account={web3.address}
-			chainId={web3.chainId}
-			connectWallet={web3.connectWallet}
-		>
-			<ChainGate chainId={provider?._network?.chainId ?? 1}>
-				<ZnsSdkProvider>
-					<ZUIProvider>
-						<App />
-					</ZUIProvider>
-				</ZnsSdkProvider>
-			</ChainGate>
-		</Web3Provider>
+			<Web3Provider
+				provider={provider}
+				account={web3.address}
+				chainId={web3.chainId}
+				connectWallet={web3.connectWallet}
+			>
+				<ChainGate chainId={provider?._network?.chainId ?? 1}>
+					<ZnsSdkProvider>
+						<ZsaleSdkProvider>
+							<ZUIProvider>
+								<App />
+							</ZUIProvider>
+						</ZsaleSdkProvider>
+					</ZnsSdkProvider>
+				</ChainGate>
+			</Web3Provider>
 	</QueryClientProvider>
 );
