049b32c52c5873e9939c6ad869646761c7558913
dominic <domw30@gmail.com>
2023-04-24 14:31:22 +1000

    fix: temporarily remove subdomain metrics stat widgets

--- src/features/view-subdomains/ViewSubdomains.tsx
+++ src/features/view-subdomains/ViewSubdomains.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { SubdomainMetrics } from './SubdomainMetrics';
 import { SubdomainTable } from './SubdomainTable';
 
 import { SubdomainViewSelector } from './selectors';
@@ -12,7 +11,6 @@ interface ViewSubdomainsProps {
 export const ViewSubdomains = ({ zna }: ViewSubdomainsProps) => {
 	return (
 		<section data-testid={SubdomainViewSelector.VIEW_SUBDOMAINS_SECTION}>
-			<SubdomainMetrics zna={zna} />
 			<SubdomainTable zna={zna} />
 		</section>
 	);
