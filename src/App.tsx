6ec7fecb8d4c55157db3c69028812b7b45270fa0
Brett <12437916+colbr@users.noreply.github.com>
2023-05-09 15:13:35 +1000

    fix: straggler files missed in merge commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,19 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 
 import { useCurrentRoute, useSubdomainDataById } from './lib/hooks';
-
 import { DynamicSizeWrapper } from './lib/util/DynamicSizeWrapper';
+
 import { Domains } from './pages/Domains';
 import { NFT } from './pages/NFT';
 
 import styles from './App.module.scss';
-import { RaffleContainer } from './features/nft-drop-raffle';
-import { BannerSeries } from './features/ui/BannerSeries/BannerSeries';
 
 export const App = () => {
 	const { isNftViewParams, currentDomainId } = useCurrentRoute();
 	const { isLoading, data: subdomains } = useSubdomainDataById(currentDomainId);
-	const enableBanner = true;
 
 	return (
 		<DynamicSizeWrapper className={styles.Container}>
-			{enableBanner && <RaffleContainer />}
 			<Switch>
 				{isNftViewParams || (!isLoading && subdomains?.length === 0) ? (
 					<NFT />
--- src/pages/Domains/Domains.tsx
+++ src/pages/Domains/Domains.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import { useCurrentRoute } from '../../lib/hooks/useCurrentRoute';
 
@@ -11,15 +11,19 @@ import {
 import { DomainsPageSelector } from './selectors';
 
 import styles from './Domains.module.scss';
+import { RaffleContainer } from '../../features/nft-drop-raffle';
 
 export const Domains: FC = () => {
 	const { currentZna, isRootDomain } = useCurrentRoute();
+
+	const enableBanner = true;
 
 	return (
 		<main
 			className={styles.Main}
 			data-testid={DomainsPageSelector.DOMAINS_PAGE_CONTAINER}
 		>
+			{enableBanner && <RaffleContainer />}
 			{!isRootDomain && (
 				<>
 					<DomainBannerContainer zna={currentZna} />
